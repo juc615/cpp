6da97bdb72ad6cec34b8af9c6fc49584ee76b1ea
juc615 <mk010615@naver.com>
2025-03-23 20:35:39 +0900

    Update main.c

---
+++ 3주차/text.c/main.c
@@ -0,0 +1,9 @@
+// main.c
+#include <stdio.h>
+#include "reverse.h"  // reverse.h 포함
+
+int main() {
+    int num = 12345;
+    printf("Reversed: %d\n", reverse(num));  // reverse 함수 호출
+    return 0;
+}
