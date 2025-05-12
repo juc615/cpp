0c3e976371aecaf07d06c635fd1b35ca3b7df90b
juc615 <mk010615@naver.com>
2025-05-12 20:48:08 +0900

    second commit

---
+++ 10/MonsterWorld3/Monster.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "Canvas.h"
+#define DIM 40
+
+class Monster {
+	string name, icon;
+	int x, y, nItem;
+	int energy;
+
+	void clip(int maxx, int maxy) {
+		if (x < 0) x = 0;
+		if (x >= maxx) x = maxx - 1;
+		if (y < 0) y = 0;
+		if (y >= maxy) y = maxy - 1;
+	}
+
+	void eat(int** map) {
+		if (map[y][x] == 1) {
+			map[y][x] = 0;
+			nItem++;
+			energy += 10;
+		}
+	}
+
+public:
+	Monster(string n = "나괴물", string i = "※", int px = 0, int py = 0)
+		: name(n), icon(i), x(px), y(py), nItem(0), energy(100) {}
+
+	~Monster() {
+		cout << "\t" << name << icon << " 물러갑니다~~~\n";
+	}
+
+	void draw(Canvas& canvas) { canvas.draw(x, y, icon); }
+
+	void move(int** map, int maxx, int maxy) {
+		if (energy <= 0) return;
+
+		switch (rand() % 8) {
+		case 0: y--; break;
+		case 1: x++; y--; break;
+		case 2: x++; break;
+		case 3: x++; y++; break;
+		case 4: y++; break;
+		case 5: x--; y++; break;
+		case 6: x--; break;
+		case 7: x--; y--; break;
+		}
+		clip(maxx, maxy);
+		eat(map);
+		energy -= 5;
+	}
+
+	void print() {
+		cout << "\t" << name << icon
+			<< " : 아이템=" << nItem
+			<< ", 에너지=" << energy << endl;
+	}
+
+	int getEnergy() const { return energy; }
+};
---
+++ 10/MonsterWorld3/MonsterWorld.h
@@ -0,0 +1,90 @@
+#pragma once
+#include "Canvas.h"
+#include "Monster.h"
+#include "Matrix.h"
+#include <windows.h>
+#define MAXMONS 8
+
+class MonsterWorld {
+	Matrix world;
+	int xMax, yMax, nMon, nMove;
+	Monster* pMon[MAXMONS];
+	Canvas canvas;
+
+	int& Map(int x, int y) { return world.elem(x, y); }
+	bool isDone() { return countItems() == 0; }
+
+	int countItems() {
+		int nItems = 0;
+		for (int y = 0; y < yMax; y++)
+			for (int x = 0; x < xMax; x++)
+				if (Map(x, y) > 0) nItems++;
+		return nItems;
+	}
+
+	void print() {
+		canvas.clear();
+		for (int y = 0; y < yMax; y++)
+			for (int x = 0; x < xMax; x++)
+				if (Map(x, y) > 0) canvas.draw(x, y, "■");
+
+		for (int i = 0; i < nMon; i++)
+			pMon[i]->draw(canvas);
+
+		canvas.print("[ Monster World (조절되는 세상) ]");
+
+		cerr << " 전체 이동 횟수 = " << nMove << endl;
+		cerr << " 남은 아이템 수 = " << countItems() << endl;
+		for (int i = 0; i < nMon; i++)
+			pMon[i]->print();
+	}
+
+	void checkStarvation() {
+		for (int i = 0; i < nMon;) {
+			if (pMon[i]->getEnergy() <= 0) {
+				delete pMon[i];
+				for (int j = i; j < nMon - 1; j++)
+					pMon[j] = pMon[j + 1];
+				nMon--;
+			}
+			else {
+				i++;
+			}
+		}
+	}
+
+public:
+	MonsterWorld(int w, int h) : world(h, w), canvas(w, h), xMax(w), yMax(h) {
+		nMon = 0;
+		nMove = 0;
+		for (int y = 0; y < yMax; y++)
+			for (int x = 0; x < xMax; x++) Map(x, y) = 1;
+	}
+	~MonsterWorld() {
+		for (int i = 0; i < nMon; i++)
+			delete pMon[i];
+	}
+	void add(Monster* m) {
+		if (nMon < MAXMONS)
+			pMon[nMon++] = m;
+	}
+	void play(int maxwalk, int wait) {
+		print();
+		cerr << " 엔터를 누르세요...";
+		getchar();
+
+		for (int i = 0; i < maxwalk; i++) {
+			for (int k = 0; k < nMon; k++)
+				pMon[k]->move(world.Data(), xMax, yMax);
+
+			nMove++;
+
+			checkStarvation();  // ⭐ 사망 처리 추가
+			print();
+
+			if (isDone() || nMon == 0) break;
+
+			Sleep(wait);
+		}
+	}
+};
